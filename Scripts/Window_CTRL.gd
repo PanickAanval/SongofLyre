d403c21fa2f1238a28bb76b655cd9007c5553212
Nick Hordijk <yourhomieniki@gmail.com>
2026-02-05 14:23:40 +0100

    Fixed platform position & file structure

--- Scripts/Window_CTRL.gd
+++ Scripts/Window_CTRL.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
-var mywin = preload("res://World_2.tscn")
+var mywin = preload("res://Scenes/World_2.tscn")
 
 func _ready():
 	var d = mywin.instantiate()
 	add_child(d)
 	d.visible = true
-	d.position = Vector2(800,800)
+	d.position = Vector2(600,600)
 	d.title = "Window 2"
 	d.size = Vector2(1200,800)
